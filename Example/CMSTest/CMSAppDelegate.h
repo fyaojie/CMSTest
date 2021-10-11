250ba6ace26dec36c79c5f2430e8bf5703abc023
冯垚杰 <yaojie.feng@symbio.com>
2021-10-11 09:52:04 +0800

    Initial commit

---
+++ Example/CMSTest/CMSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CMSAppDelegate.h
+//  CMSTest
+//
+//  Created by 562925462@qq.com on 10/11/2021.
+//  Copyright (c) 2021 562925462@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CMSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
